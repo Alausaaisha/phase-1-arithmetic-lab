9fe70305a7abfdb4fe7a6ff34df939d9c0ccef71
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-09 23:45:27 +0100

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Number.isInteger(Math.random() * 10) + 1;
+
+let num3 = 64;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(10, 12, 14, 16,18, 20);
